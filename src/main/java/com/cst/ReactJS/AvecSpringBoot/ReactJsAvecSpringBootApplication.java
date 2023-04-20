58c005740892b60fd059c0f3ab8b7614e7c97242
Christ_YAO <christyao1299@gmail.com>
2023-04-20 22:08:10 +0100

    update spring application
    update spring application

---
+++ src/main/java/com/cst/ReactJS/AvecSpringBoot/ReactJsAvecSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cst.ReactJS.AvecSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactJsAvecSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactJsAvecSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cst/ReactJS/AvecSpringBoot/ReactJsAvecSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cst.ReactJS.AvecSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactJsAvecSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
