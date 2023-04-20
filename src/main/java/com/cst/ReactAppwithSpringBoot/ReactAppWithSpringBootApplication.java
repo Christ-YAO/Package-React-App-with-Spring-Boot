e4317f24c38fbbc3510f2294e767e532c9f61205
Christ_YAO <christyao1299@gmail.com>
2023-04-20 16:22:38 +0100

    Created spring application
    Created spring application

---
+++ src/main/java/com/cst/ReactAppwithSpringBoot/ReactAppWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cst.ReactAppwithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAppWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAppWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cst/ReactAppwithSpringBoot/ReactAppWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cst.ReactAppwithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAppWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
